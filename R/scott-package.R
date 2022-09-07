8a689bf5c35b351d129ecd86dc38748880f2eda7
Fabricia Nascimento <thednainus@yahoo.com>
2022-09-07 15:01:39 +0100

    fixed bug for heterogeneous sampling time

---
+++ R/scott-package.R
@@ -0,0 +1,19 @@
+#' Scott: Structured Coalescent Transmission Tree simulation
+#'
+#' write here what is scott
+#'
+#' @section add description about scott here:
+#' ~~ An overview of how to use the package, including the most important ~~
+#'   ~~ functions ~~
+#'
+#' @references ~~ Literature or other references for background information ~~
+#'   ~~ Optionally other standard keywords, one per line, from file KEYWORDS in ~~
+#'     ~~ the R documentation directory ~~
+#' seealso: ~~ Optional links to other man pages, e.g. ~~
+#' ~~ simple examples of the most important functions ~~
+#'
+#' @keywords package
+#'
+#' @docType package
+#' @name Scott
+NULL
